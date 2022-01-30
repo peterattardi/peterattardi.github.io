6c0f8b07f69863a1e89216490ee0b4c2c0c7b50c
Pietro Attardi <peter@Pietros-MacBook-Air.local>
2022-01-30 15:10:40 +0100

    main, footer

--- scripts/animations.js
+++ scripts/animations.js
@@ -2,7 +2,12 @@ const sleep = async (ms) => {
   return new Promise((resolve) => setTimeout(resolve, ms));
 };
 
-const words = ["coglione", "minchione", "secchione"];
+const words = [
+  "software developer",
+  "tech enthusiast",
+  "human being",
+  "a lot of things",
+];
 let count = 0;
 let index = 0;
 let currentText = "";
@@ -30,18 +35,44 @@ const typeAnimation = async () => {
 
 tl = gsap.timeline({ defaults: { ease: "power2.out" } });
 tl.to("body", { css: { overflow: "hidden" } });
-tl.to("h1", { y: 0, duration: 0.5, stagger: 1 }, "-=2");
+tl.to(".loading-slider, main, footer", { css: { display: "none" } });
+tl.to("h1", { y: 0, duration: 0.5, stagger: 1 }, "-=5");
 
 tl.fromTo(
   ".loading-text h1",
   { color: "white" },
-  { color: "rgb(256,99,71)", duration: 2, stagger: 1 },
+  { color: "rgb(256,99,71)", duration: 0.5, stagger: 1 },
   "-=1.5"
 );
-tl.to(".loading-slider", { y: "-100%", duration: 2 });
-tl.from(".loading-animations", { y: 0, duration: 3.5 }, "-=1.5");
-tl.fromTo("header", { opacity: 0 }, { opacity: 1, duration: 3 }, "-=2.5");
-tl.to("body", { css: { overflow: "visible" } }, "-=2.5");
+tl.to(".loading-slider, main, footer", { css: { display: "flex" } });
+tl.to(".loading-slider", { y: "-100%", duration: 1 });
+tl.to("body", { css: { overflow: "visible" } });
+tl.from(".loading-animations", { y: 0, duration: 3 }, "-=1.5");
+tl.fromTo("header", { opacity: 0 }, { opacity: 1, duration: 3 }, "-=2");
+tl.to(".loading-animation, .loading-slider, .loading-text", {
+  css: { display: "none" },
+});
+
 tl.add(() => {
-  setInterval(typeAnimation, 250);
+  setInterval(typeAnimation, 175);
 }, "-=2.5");
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to(".details__grid", {
+  scrollTrigger: ".details__grid",
+  opacity: 1,
+  duration: 5,
+});
+
+gsap.to(".projects__grid", {
+  scrollTrigger: ".projects__grid",
+  opacity: 1,
+  duration: 3,
+});
+
+gsap.to(".email", {
+  scrollTrigger: ".email",
+  opacity: 1,
+  duration: 3,
+});
--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,18 @@
 window.onload = function () {
   window.scrollTo(0, 0);
+
+  const copyBtn = document.querySelector(".email div");
+  copyBtn.addEventListener("click", () => {
+    /* Get the text field */
+    var copyText = document.getElementById("myInput");
+
+    /* Copy the text inside the text field */
+    navigator.clipboard.writeText(copyText.value);
+
+    let tootltiptext = document.querySelector(".tooltiptext");
+    tootltiptext.textContent = "Copied";
+    setTimeout(() => {
+      tootltiptext.textContent = "Copy to Clipboard";
+    }, 4000);
+  });
 };
